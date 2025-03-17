059491d360c75849065d53bf46d3b8c8803011b8
Ethan Minja <79723001+Ethan-Bit-256@users.noreply.github.com>
2025-03-17 15:49:13 +0300

    Comments
    Added some comments that I forgot

--- mathfuncs.py
+++ mathfuncs.py
@@ -1,9 +1,13 @@
-def add(*args):
+import math
+
+""" The addition function. You can take any number of arguments and add them. """
+def sum(*args):
     result = 0
     for i in args:
         result = result + i
     return result
 
+""" The subtraction function. You can take any number of arguments and subtract them (starting from the first argument). """
 def sub(*args):
     number = args[0]
 
@@ -11,12 +15,14 @@ def sub(*args):
         number = number - i
     return number
 
+""" The multiplication function. You can take any number of arguments and multiply them. """
 def mul(*args):
     result = 1
     for i in args:
         result = result * i
     return result
 
+""" The division function. You can take any number of arguments and divide them (starting from the fisrt argument). """
 def div(*args):
     number = args[0]
 
