34d7e6670a2a39f8c5490abb766cd9df5bdaae4a
Ethan Minja <79723001+Ethan-Bit-256@users.noreply.github.com>
2025-04-03 19:14:20 +0300

    Example
    A simple example program to showcase the library's small capabillites.

---
+++ example1.py
@@ -0,0 +1,7 @@
+from mathfuncs import *
+
+print(f"1 + 2.2 + 3.33 = {sum(1, 2.2, 3.33)}")
+print(f"10 - 5 - 3.5 = {sub(10, 5, 3.5)}")
+print(f"3 * 4.5 * 2 = {mul(3, 4.5, 2)}")
+print(f"40 / 100 = {div(40, 100)}")
+print(f"5! = {factorial(5)}")
